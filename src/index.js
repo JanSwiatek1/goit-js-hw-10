238947a71b7c48738e0f8aceb7d3b80d5baefd97
Jan Świątek <swiatek-jan@wp.pl>
2023-12-12 17:22:44 +0100

    variables

---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import axios from "axios";
+
+const breedSelect = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
